841cca01052e0706fd1d3da7ae552f37da3eab2f
Angelo Naselli <anaselli@linux.it>
2019-05-18 16:24:44 +0200

    Added HelpDialog and HelpInfoBase to manage Help funtion in applications

--- test/testHelpDialog.py
+++ test/testHelpDialog.py
@@ -11,7 +11,9 @@ Author:  Angelo Naselli <anaselli@linux.it>
 @package manatools
 '''
 
-import manatools.ui.basedialog as basedialog
+import manatools.basehelpinfo as helpdata
+import manatools.ui.helpdialog as helpdialog
+
 import yui
 import time
 
@@ -21,29 +23,11 @@ import time
 ## 
 ######################################################################
 
-class HelpInfoBase:
+
+
+class HelpInfo(helpdata.HelpInfoBase):
   def __init__(self):
-    pass
-
-  def show(self, info_to_show):
-    '''
-    super class must implement show to return the right string to show
-    into dialog
-    @param info_to_show: a kind of index of what to show, it depends on implementation
-    '''
-    raise NotImplementedError("show is not implemented")
-
-  def home(self):
-    '''
-    super class must implement show to return the very first info to show
-    into dialog, such as index for instance. Than index could be anchored and anchors passed
-    to show() to display related content
-    '''
-    raise NotImplementedError("home is not implemented")
-
-class HelpInfo(HelpInfoBase):
-  def __init__(self):
-    HelpInfoBase.__init__(self)
+    helpdata.HelpInfoBase.__init__(self)
     index1 = '<b>%s</b>'%self._formatLink("Title 1", 'title1')
     index2 = '<b>%s</b>'%self._formatLink("Title 2", 'titleindex2')
     self.text = { 'home': "This text explain how to use manatools Help Dialog. <br><br>%s <br>%s"%(index1, index2),
@@ -76,61 +60,10 @@ class HelpInfo(HelpInfoBase):
     return self.text['home']
 
 
-class HelpDialog(basedialog.BaseDialog):
-  def __init__(self, info):
-    basedialog.BaseDialog.__init__(self, "Help dialog", "", basedialog.DialogType.POPUP, 80, 10)
-    #### TODO check instance of HelpInfoBase
-    self.info = info
-    
-  def UIlayout(self, layout):
-    '''
-    layout implementation called in base class to setup UI
-    '''
-    
-    # Let's test a Menu widget
-    menu = self.factory.createMenuButton(self.factory.createLeft(layout), "Test &menu")
-    qm = yui.YMenuItem("&Quit")
-    menu.addItem(qm)
-    menu.rebuildMenuTree()
-    sendObjOnEvent=True
-    self.eventManager.addMenuEvent(qm, self.onQuitEvent, sendObjOnEvent)
-    # URL events are sent as YMenuEvent by libyui
-    self.eventManager.addMenuEvent(None, self.onURLEvent, False)
-
-    self.text = self.factory.createRichText(layout, "", False)
-    self.text.setValue(self.info.home())
-
-    #let's test some buttons
-    align = self.factory.createRight(layout)
-
-    # Let's test a quitbutton (same handle as Quit menu)
-    self.quitButton = self.factory.createPushButton(align, "&Quit")
-    self.eventManager.addWidgetEvent(self.quitButton, self.onQuitEvent, sendObjOnEvent)
-    
-    # Let's test a cancel event
-    self.eventManager.addCancelEvent(self.onCancelEvent)
-    
-  def onCancelEvent(self) :
-    print ("Got a cancel event")
-
-  def onQuitEvent(self, obj) :
-    if isinstance(obj, yui.YItem):
-      print ("Quit menu pressed")
-    else:
-      print ("Quit button pressed")
-    # BaseDialog needs to force to exit the handle event loop 
-    self.ExitLoop()
-
-  def onURLEvent(self, mEvent):
-    print("onURLEvent")
-    url = mEvent.id()
-    if url:
-      self.text.setValue(self.info.show(url))
-
 if __name__ == '__main__':
 
   info = HelpInfo()
-  td = HelpDialog(info)
+  td = helpdialog.HelpDialog(info)
   td.run()
   
   
